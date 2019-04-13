31eb0dc701a3c6e9b92486f10512e1213337b5ac
Eduardo Lopes <eduardo.lopes@neemu.com>
2019-04-13 09:17:14 -0400

    add proxy pattern to reflect model change

--- client/src/controllers/NegociacaoController.js
+++ client/src/controllers/NegociacaoController.js
@@ -7,12 +7,24 @@ import MensagemView from '../views/MensagemView';
 
 class NegociacaoController {
   constructor() {
+    const self = this;
     const $ = document.querySelector.bind(document);
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
 
-    this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
+    this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+      get: (target, prop, receiver) => {
+        if (['adiciona', 'esvazia'].indexOf(prop) !== -1 && typeof (target[prop]) === typeof (Function)) {
+          return function () {
+            // eslint-disable-next-line prefer-rest-params
+            Reflect.apply(target[prop], target, arguments);
+            self._negociacoesView.update(target);
+          };
+        }
+        return Reflect.get(target, prop, receiver);
+      },
+    });
 
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
     this._negociacoesView.update(this._listaNegociacoes);
